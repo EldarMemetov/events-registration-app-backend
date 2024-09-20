3679cb32fe664b8d111e32ff8eba8e1fb7596a92
EldarMemetov <eldarm9347@gmail.com>
2024-09-20 16:47:33 +0200

    update

--- src/routers/participants.js
+++ src/routers/participants.js
@@ -7,6 +7,6 @@ import {
 const router = express.Router();
 
 router.post('/', registerParticipant);
-router.get('/', getAllParticipants); // Добавь этот маршрут
+router.get('/', getAllParticipants); 
 
 export default router;
